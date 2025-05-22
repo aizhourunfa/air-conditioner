c8b62ad3f03ca906849ae356fcc726086b44da31
周润发演示 <aizhourunfa@gmail.com>
2025-05-22 20:18:50 +0800

    Update Copyright.tsx

--- packages/react/src/components/layouts/Copyright.tsx
+++ packages/react/src/components/layouts/Copyright.tsx
@@ -7,13 +7,14 @@ const Copyright: React.FC = () => {
         © 云游君
       </div>
       <div className="ac-text flex justify-center items-center m-2">
+        由周润发维护： 
         <a
           href="https://blog.zrf.me/"
           target="_blank"
           rel="noopener noreferrer"
-          className="hover:underline text-blue-500"
+          className="hover:underline ml-1 text-blue-500"
         >
-          周润发
+          BLOG
         </a>
       </div>
     </div>
