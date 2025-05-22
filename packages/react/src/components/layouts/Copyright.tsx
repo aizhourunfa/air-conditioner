c43ef079a274e2111361657cdddcd8855bedd2eb
周润发演示 <aizhourunfa@gmail.com>
2025-05-22 20:11:26 +0800

    Update Copyright.tsx

--- packages/react/src/components/layouts/Copyright.tsx
+++ packages/react/src/components/layouts/Copyright.tsx
@@ -4,7 +4,18 @@ const Copyright: React.FC = () => {
   return (
     <div className="opacity-80 text-xs text-center mt-8">
       <div className="ac-text flex justify-center items-center m-2">
-        © Yun Air Conditioner
+        © 云游君
+      </div>
+      <div className="ac-text flex justify-center items-center m-2">
+        博客： 
+        <a
+          href="https://blog.zrf.me/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover:underline ml-1 text-blue-500"
+        >
+          周润发
+        </a>
       </div>
     </div>
   )
